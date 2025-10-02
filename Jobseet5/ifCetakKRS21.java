1ad4db2107b821ff273c7881ad9a390b288b81bd
wasik <wasikwwasikm@gmail.com>
2025-10-02 11:55:33 +0700

    tugas

---
+++ Jobseet5/ifCetakKRS21.java
@@ -0,0 +1,9 @@
+package Jobseet5;
+import java.util.Scanner;
+
+public class ifCetakKRS21 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+       
+    }
+}
