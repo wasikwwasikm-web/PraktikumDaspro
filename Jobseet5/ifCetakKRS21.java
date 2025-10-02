3622cac95129307dd2b91943539a2c0cea713e51
wasik <wasikwwasikm@gmail.com>
2025-10-02 16:05:16 +0700

    Modifikasi Percobaan 1

--- Jobseet5/ifCetakKRS21.java
+++
@@ -1,9 +0,0 @@
-package Jobseet5;
-import java.util.Scanner;
-
-public class ifCetakKRS21 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-       
-    }
-}
---
+++ jobsheet5/ifCetakKRS21.java
@@ -0,0 +1,17 @@
+package jobsheet5;
+import java.util.Scanner;
+public class ifCetakKRS21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         System.out.println("--- Cetak KRS SIAKAD ---");
+         System.out.print("Apakah UKT sudah lunas? (true/false): ");
+            boolean uktLunas = sc.nextBoolean();
+            if (uktLunas) {
+                System.out.println("Pembayaran UKT terverifikasi");
+                System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+            }
+            }
+
+}
+
+
