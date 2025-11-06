1b8609ac403c743a7a96e9a85b618cb230e755f2
wasik <wasikwwasikm@gmail.com>
2025-11-06 10:44:14 +0700

    nomer5

--- jobsheet9/ArrayBilangan20.java
+++ jobsheet9/ArrayBilangan20.java
@@ -2,7 +2,7 @@ package jobsheet9;
 public class ArrayBilangan20 {
     public static void main(String[] args) {
         int[] bil = {5, 13, -7, 17};
-        for (int i = 0; i <= 4; i++) {
+        for (int i = 0; i < 4; i++) {
             System.out.println(bil[i]);
         }
     }
