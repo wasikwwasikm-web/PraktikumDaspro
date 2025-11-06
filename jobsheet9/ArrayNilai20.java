8f4cb44fd0bc90fb676bb22152245426e30601d8
wasik <wasikwwasikm@gmail.com>
2025-11-06 15:54:02 +0700

    p2t5

--- jobsheet9/ArrayNilai20.java
+++ jobsheet9/ArrayNilai20.java
@@ -4,12 +4,17 @@ public class ArrayNilai20 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai Akhir ke-" + i + ": ");
             nilaiAkhir[i] = input.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai Akhir ke-" + i + " Adalah " + nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70){
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }
+            else{
+                System.out.println("Mahasiswa ke-"+i+" Tidak lulus!");
+            }
         }
         input.close();
     }
